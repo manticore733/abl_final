f90932dbf1437ecccbf84b362f935708c938eda1
Pranav Batki <pranavbatki4@gmail.com>
2026-05-02 00:08:35 +0530

    removed comments

--- server/Routes/studentRoutes.js
+++ server/Routes/studentRoutes.js
@@ -1,36 +1,11 @@
-// import express from "express";
-// import { submitStudentEntry, getStudentInfo, getStudentActivityInfo } from "../controller/studentController.js";
-
-// const router = express.Router();
-
-// router.post("/make-entry", submitStudentEntry);
-// router.get("/profile", getStudentInfo);
-// router.get("/student-activity", getStudentActivityInfo);
-
-// export default router;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import express from "express";
-import { submitStudentEntry, getStudentInfo, getStudentActivityInfo,  addStudentActivitySummary,
+import {
+    submitStudentEntry, getStudentInfo, getStudentActivityInfo, addStudentActivitySummary,
     getAllStudentActivitySummaries,
     getStudentActivityByRollNumber,
     updateStudentActivitySummary,
-    deleteStudentActivitySummary,getStudentDetailsByRollNumber } from "../controller/studentController.js";
+    deleteStudentActivitySummary, getStudentDetailsByRollNumber
+} from "../controller/studentController.js";
 
 const router = express.Router();
 
